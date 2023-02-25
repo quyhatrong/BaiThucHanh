9540b4911f7378059a47404c1f0ba37260cba76e
quyhatrong <haquy1910@gmail.com>
2023-02-25 22:51:40 +0700

    update 2102

--- BT0702/App.java
+++ BT0702/App.java
@@ -1,4 +1,6 @@
-public class Main {
+package baiThucHanh.BT0702;
+
+public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
