e77a914788f867991dda12e4004d8d5d7416b141
quyhatrong <haquy1910@gmail.com>
2023-02-28 09:56:56 +0700

    Update code

--- 28-02/Slide47.java
+++ 28-02/Slide47.java
@@ -2,19 +2,24 @@ import java.util.Scanner;
 
 public class Slide47 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int tong = 0;
-
-        System.out.println("Nhap vao so nguyen:");
-        int a = sc.nextInt();
-
-        do {
+        try (Scanner sc = new Scanner(System.in)) {
+            int tong = 0;
+            int a;
+            System.out.println("Nhap vao so nguyen a: ");
+            a = sc.nextInt();
             tong = tong + a;
-            System.out.println("Nhap vao so nguyen tiep theo: ");
-            int b = sc.nextInt();
-            
-            break;
+            System.out.println("Tong hien tai la : " + tong);
 
-        } while (tong > 0);
+            do {
+                System.out.println("Nhap vao so nguyen tiep theo: ");
+                a = sc.nextInt();
+                tong = tong + a;
+                if (tong < 100) {
+                    System.out.println("Tong hien tai la : " + tong);
+                }
+
+            } while (tong < 100);
+            System.out.println("Tong cac so da nhap la: " + tong);
+        }
     }
 }
---
+++ 28-02/Slide55.java
@@ -0,0 +1,9 @@
+public class Slide55 {
+    public static void main(String[] args) {
+        System.out.print("Cac so nguyen duong chia het cho 5 va <=20 : ");
+        for (int n = 0; n <= 20;) {
+            System.out.print(n + " ");
+            n += 5;
+        }
+    }
+}
