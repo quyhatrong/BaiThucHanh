6b85550444adf06d33b532dadc7620a8b5e7a30b
quyhatrong <haquy1910@gmail.com>
2023-03-22 21:42:14 +0700

    xoa cac file baithuchanh cu

--- 28-02/Slide55.java
+++
@@ -1,9 +0,0 @@
-public class Slide55 {
-    public static void main(String[] args) {
-        System.out.print("Cac so nguyen duong chia het cho 5 va <=20 : ");
-        for (int n = 5; n <= 20;) {
-            System.out.print(n + " ");
-            n += 5;
-        }
-    }
-}
